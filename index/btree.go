9357f35846adb1bacc8144878aa98be2e5842209
jieqiyue <jieqiyue123@gamil.com>
2024-10-24 18:14:48 +0800

    添加索引迭代器

--- index/btree.go
+++ index/btree.go
@@ -1,6 +1,8 @@
 package index
 
 import (
+	"bytes"
+	"sort"
 	"sync"
 
 	"bitcask-go/data"
@@ -55,4 +57,84 @@ func (bt *BTree) Delete(key []byte) bool {
 	}
 
 	return true
+}
+
+func (bt *BTree) Size() int {
+	return bt.tree.Len()
+}
+
+func (bt *BTree) Iterator(reverse bool) Iterator {
+	if bt.tree == nil {
+		return nil
+	}
+	bt.lock.RLock()
+	defer bt.lock.RUnlock()
+	return newBTreeIterator(bt.tree, reverse)
+}
+
+// BTree 索引迭代器
+// 如果是BTree的话，就会把所有内存索引都取出来，然后放到数组中。然后定义了一个currentIndex，通过这个index能够做各种操作。
+type btreeIterator struct {
+	currIndex int     // 当前遍历的下标位置
+	reverse   bool    // 是否是反向遍历
+	values    []*Item // key+位置索引信息
+}
+
+func newBTreeIterator(tree *btree.BTree, reverse bool) *btreeIterator {
+	var idx int
+	values := make([]*Item, tree.Len())
+
+	// 将所有的数据存放到数组中
+	saveValues := func(it btree.Item) bool {
+		values[idx] = it.(*Item)
+		idx++
+		return true
+	}
+	if reverse {
+		tree.Descend(saveValues)
+	} else {
+		tree.Ascend(saveValues)
+	}
+
+	return &btreeIterator{
+		currIndex: 0,
+		reverse:   reverse,
+		values:    values,
+	}
+}
+
+func (bti *btreeIterator) Rewind() {
+	bti.currIndex = 0
+}
+
+func (bti *btreeIterator) Seek(key []byte) {
+	if bti.reverse {
+		bti.currIndex = sort.Search(len(bti.values), func(i int) bool {
+			return bytes.Compare(bti.values[i].key, key) <= 0
+		})
+	} else {
+		bti.currIndex = sort.Search(len(bti.values), func(i int) bool {
+			return bytes.Compare(bti.values[i].key, key) >= 0
+		})
+	}
+}
+
+func (bti *btreeIterator) Next() {
+	bti.currIndex += 1
+}
+
+func (bti *btreeIterator) Valid() bool {
+	return bti.currIndex < len(bti.values)
+}
+
+func (bti *btreeIterator) Key() []byte {
+	return bti.values[bti.currIndex].key
+}
+
+func (bti *btreeIterator) Value() *data.LogRecordPos {
+	return bti.values[bti.currIndex].pos
+}
+
+func (bti *btreeIterator) Close() {
+	bti.values = nil
 }
